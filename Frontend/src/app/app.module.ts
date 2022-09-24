089c826892947dfc99d3e67e05405aca33b7ba4b
giulio <terigigiulio@gmail.com>
2022-09-24 22:31:40 +0200

    + Modifiche sull'header + Aggiunto il service per gestire il login/logout dell'utente + Aggiunti component e View dell'utente durante il login + Creazione del componente CardContainer per uniformare il layout

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,30 +4,42 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {TheHeaderComponent} from './view/the-header/the-header.component';
+import {TheHeaderComponent} from './component/the-header/the-header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from '@angular/material/button';
 import {RouterModule} from "@angular/router";
 import {IndexComponent} from './view/index/index.component';
+import {LoginComponent} from './view/login/login.component';
+import {ContainerCardComponent} from './component/container-card/container-card.component';
+import {UserLoginComponent} from './component/user-login/user-login.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TheHeaderComponent,
     IndexComponent,
+    LoginComponent,
+    ContainerCardComponent,
+    UserLoginComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    // RouterModule.forRoot([
-    //   {path: '', component: ProductListComponent},
-    //   {path: 'products/:productId', component: ProductDetailsComponent},
-    // ])
+    RouterModule.forRoot([
+      {path: '', component: IndexComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'library', component: IndexComponent},
+    ]),
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
