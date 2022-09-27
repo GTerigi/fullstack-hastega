283cfcc060738ac4879d26aaab610c3ccb9856c6
giulio <terigigiulio@gmail.com>
2022-09-27 18:36:36 +0200

    Aggiunte il caricamento delle icone

--- Frontend/src/app/component/user-login/user-login.component.ts
+++ Frontend/src/app/component/user-login/user-login.component.ts
@@ -1,24 +1,55 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {User} from "../../interface/User";
+import {UserService} from "../../services/user-service/user.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.scss']
+  styleUrls: ['./user-login.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      state('hide', style({opacity: 0})),
+      state('visible', style({opacity: 1})),
+      transition('hide => visible', animate('500ms ease-in')),
+    ]),
+  ]
 })
 export class UserLoginComponent implements OnInit {
   @Input() user!: User; // TypeScript's Strict Class Initialization
   @Input() last!: boolean; //TypeScript's Strict Class Initialization
   @Output() onLoginSelection: EventEmitter<User> = new EventEmitter();
 
-  constructor() {
+  iconIsLoading: boolean = true;
+  iconSrc!: string | ArrayBuffer | null;
+  fallbackIcon: string = './../assets/round-default-icon.png';
+  transitionState = 'hide';
+
+  constructor(private userService: UserService) {
   }
 
   ngOnInit(): void {
+    this.userService.getIcon(this.user.id).subscribe({
+      next: (data) => {
+        this.createImageFromBlob(data);
+      }
+    })
   }
 
   submitLogin() {
     this.onLoginSelection.emit(this.user);
   }
 
+  private createImageFromBlob(image: Blob) {
+    let reader = new FileReader();
+    this.iconIsLoading = false;
+    reader.addEventListener("load", () => {
+      this.iconSrc = reader.result;
+      this.transitionState = 'visible';
+    }, false);
+
+    if (image) {
+      reader.readAsDataURL(image);
+    }
+  }
 }
